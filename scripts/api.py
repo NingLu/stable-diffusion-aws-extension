c9ed083da7cde89f7b779596ef6533903854bd7b
Xiujuan Li <xiujuali@amazon.com>
2023-07-13 19:02:49 +0800

    feature: opt

--- scripts/api.py
+++ scripts/api.py
@@ -165,42 +165,42 @@ def sagemaker_api(_, app: FastAPI):
         """
         print('-------invocation------')
 
-        def show_slim_dict(payload):
-            pay_type = type(payload)
-            if pay_type is dict:
-                for k, v in payload.items():
-                    print(f"{threading.current_thread().ident}_{threading.current_thread().name}_______{k}")
-                    show_slim_dict(v)
-            elif pay_type is list:
-                for v in payload:
-                    print(f"{threading.current_thread().ident}_{threading.current_thread().name}_______list")
-                    show_slim_dict(v)
-            elif pay_type is str:
-                if len(payload) > 50:
-                    print(f"{threading.current_thread().ident}_{threading.current_thread().name}_______ : {len(payload)} contents")
-                else:
-                    print(f"{threading.current_thread().ident}_{threading.current_thread().name}_______ : {payload}")
-            else:
-                print(f"{threading.current_thread().ident}_{threading.current_thread().name}_______ : {payload}")
+        # def show_slim_dict(payload):
+        #     pay_type = type(payload)
+        #     if pay_type is dict:
+        #         for k, v in payload.items():
+        #             print(f"{threading.current_thread().ident}_{threading.current_thread().name}_______{k}")
+        #             show_slim_dict(v)
+        #     elif pay_type is list:
+        #         for v in payload:
+        #             print(f"{threading.current_thread().ident}_{threading.current_thread().name}_______list")
+        #             show_slim_dict(v)
+        #     elif pay_type is str:
+        #         if len(payload) > 50:
+        #             print(f"{threading.current_thread().ident}_{threading.current_thread().name}_______ : {len(payload)} contents")
+        #         else:
+        #             print(f"{threading.current_thread().ident}_{threading.current_thread().name}_______ : {payload}")
+        #     else:
+        #         print(f"{threading.current_thread().ident}_{threading.current_thread().name}_______ : {payload}")
 
         print(f"{threading.current_thread().ident}_{threading.current_thread().name}_______task is {req.task}")
         print(f"{threading.current_thread().ident}_{threading.current_thread().name}_______checkpoint_info is {req.checkpoint_info}")
         print(f"{threading.current_thread().ident}_{threading.current_thread().name}_______models is {req.models}")
         print(f"{threading.current_thread().ident}_{threading.current_thread().name}_______txt2img_payload is: ")
         txt2img_payload = {} if req.txt2img_payload is None else json.loads(req.txt2img_payload.json())
-        show_slim_dict(txt2img_payload)
+        print(txt2img_payload)
         print(f"{threading.current_thread().ident}_{threading.current_thread().name}_______img2img_payload is: ")
         img2img_payload = {} if req.img2img_payload is None else json.loads(req.img2img_payload.json())
-        show_slim_dict(img2img_payload)
+        print(img2img_payload)
         print(f"{threading.current_thread().ident}_{threading.current_thread().name}_______extra_single_payload is: ")
         extra_single_payload = {} if req.extras_single_payload is None else json.loads(req.extras_single_payload.json())
-        show_slim_dict(extra_single_payload)
+        print(extra_single_payload)
         print(f"{threading.current_thread().ident}_{threading.current_thread().name}_______extra_batch_payload is: ")
         extra_batch_payload = {} if req.extras_batch_payload is None else json.loads(req.extras_batch_payload.json())
-        show_slim_dict(extra_batch_payload)
+        print(extra_batch_payload)
         print(f"{threading.current_thread().ident}_{threading.current_thread().name}_______interrogate_payload is: ")
         interrogate_payload = {} if req.interrogate_payload is None else json.loads(req.interrogate_payload.json())
-        show_slim_dict(interrogate_payload)
+        print(interrogate_payload)
         print(f"{threading.current_thread().ident}_{threading.current_thread().name}_______db_create_model_payload is: ")
         print(f"{threading.current_thread().ident}_{threading.current_thread().name}_______{req.db_create_model_payload}")
         print(f"{threading.current_thread().ident}_{threading.current_thread().name}_______merge_checkpoint_payload is: ")
